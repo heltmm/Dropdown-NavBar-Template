6a5767653f8c6d2ab7d798fb46dfe256e0832b9f
Mark <heltmm@gmail.com>
2017-12-02 22:33:43 -0800

    add avatar to edit profile, choose a default image, add pulblic/system to gitignore

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/default.png"
-   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/default.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
