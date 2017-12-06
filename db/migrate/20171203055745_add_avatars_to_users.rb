2cae819e29ba8bbf2b9cb7b36b7982e52b931e20
Mark <heltmm@gmail.com>
2017-12-06 07:41:08 -0800

    change migration format

--- db/migrate/20171203055745_add_avatars_to_users.rb
+++ db/migrate/20171203055745_add_avatars_to_users.rb
@@ -1,11 +1,7 @@
 class AddAvatarsToUsers < ActiveRecord::Migration[5.1]
-  def self.up
+  def change
     change_table :users do |t|
       t.attachment :avatar
     end
-  end
-
-  def self.down
-    drop_attached_file :users, :avatar
   end
 end
